a637b5f3a9cc3f54f1d4a1ed41aa96cdef2fb7d9
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2023-10-15 17:02:59 -0300

    Project created

---
+++ ActivityJournal/ActivityJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityJournalApp.swift
+//  ActivityJournal
+//
+//  Created by Giovanne Bressam on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ActivityJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
