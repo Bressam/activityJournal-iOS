8f8bef6a32719bcd67066d1cbd8819690add5473
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2023-10-16 11:20:36 -0300

    Cleaned chartView code

--- ActivityJournal/Scenes/ActivitiesCharts/Charts/ActivityChartView.swift
+++ ActivityJournal/Scenes/ActivitiesCharts/Charts/ActivityChartView.swift
@@ -13,47 +13,58 @@ struct ActivityChartView: View {
     
     var body: some View {
         if activityByCategory.monthlyData.isEmpty {
+            emptyDataView
+                .containerRelativeFrame(.horizontal)
+        } else {
+            activityLogChartView
+                .padding(16)
+                .containerRelativeFrame(.horizontal)
+        }
+    }
+    
+    var emptyDataView: some View {
+        ZStack {
             ContentUnavailableView(label: {
-                Label("No content for category: \(String(describing:activityByCategory.category))",
+                Label("No content for category: \(String(describing: activityByCategory.category))",
                       systemImage: "chart.line.uptrend.xyaxis.circle")
+                .symbolEffect(.pulse.byLayer)
             }, description: {
                 Text("New data will appear when logged at the activity details.")
             })
-            .containerRelativeFrame(.horizontal)
-        } else {
-            VStack(alignment: .leading) {
-                Text( "\(activityByCategory.category.description.capitalized)")
-                    .font(.title)
+        }.background(activityByCategory.category.chartDataColor.opacity(0.8).gradient)
+    }
+    
+    var activityLogChartView: some View {
+        VStack(alignment: .leading) {
+            Text( "\(activityByCategory.category.description.capitalized)")
+                .font(.title)
+                .foregroundStyle(activityByCategory.category.chartDataColor.gradient)
+            Chart {
+                ForEach(activityByCategory.monthlyData) { monthActivity in
+                    BarMark(x: .value("Month", monthActivity.date, unit: .month),
+                            y: .value("Logs", monthActivity.activitiesCount))
                     .foregroundStyle(activityByCategory.category.chartDataColor.gradient)
-                Chart {
-                    ForEach(activityByCategory.monthlyData) { monthActivity in
-                        BarMark(x: .value("Month", monthActivity.date, unit: .month),
-                                y: .value("Logs", monthActivity.activitiesCount))
-                        .foregroundStyle(activityByCategory.category.chartDataColor.gradient)
-                    }
-
-                    if let monthlyGoal = activityByCategory.monthlyGoal {
-                        let markerColor = activityByCategory.category.chartGoalMarkerColor
-                        RuleMark(y: .value("Monthly Goal", monthlyGoal))
-                            .foregroundStyle(markerColor)
-                            .lineStyle(StrokeStyle(lineWidth:1, dash: [5]))
-                            .annotation(alignment: .leading) {
-                                Text("Goal")
-                                    .font(.subheadline.bold())
-                                    .foregroundStyle(markerColor)
-                            }
-                    }
+                }
+                
+                if let monthlyGoal = activityByCategory.monthlyGoal {
+                    let markerColor = activityByCategory.category.chartGoalMarkerColor
+                    RuleMark(y: .value("Monthly Goal", monthlyGoal))
+                        .foregroundStyle(markerColor)
+                        .lineStyle(StrokeStyle(lineWidth:1, dash: [5]))
+                        .annotation(alignment: .leading) {
+                            Text("Goal")
+                                .font(.subheadline.bold())
+                                .foregroundStyle(markerColor)
+                        }
                 }
             }
-            .padding()
-            .containerRelativeFrame(.horizontal)
         }
     }
 }
 
 #Preview {
     let calendar = Calendar(identifier: .gregorian)
-    let monthlyActivities: [MonthActivities] = [
+    var monthlyActivities: [MonthActivities] = [
         .init(activitiesCount: 30,
               date: calendar.makeDate(year: 2023,
                                       month: 1)),
@@ -70,7 +81,11 @@ struct ActivityChartView: View {
               date: calendar.makeDate(year: 2023,
                                       month: 8))
     ]
-    return ActivityChartView(activityByCategory: .init(category: .finacesSaving,
+    
+    // Force empty data
+    monthlyActivities = []
+
+    return ActivityChartView(activityByCategory: .init(category: .sport,
                                                        monthlyData: monthlyActivities,
                                                        monthlyGoal: 25))
 }
